e65f0e6245453d527aa4888564b13e2ca23ea04f
shani barak <shanibrk@gmail.com>
2022-12-26 10:25:21 +0200

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="links">
+        <Link to="/">Shop</Link>
+        <Link to="/cart">Cart</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
